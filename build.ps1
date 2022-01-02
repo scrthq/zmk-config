814885e9d677543917d416c1811ccda2f46011ce
Nate Ferrell <nate@scrthq.com>
2022-01-01 21:59:12 -0600

    added defines.dtsi

--- build.ps1
+++ build.ps1
@@ -1,36 +1,42 @@
 function build() {
-  $START = Get-Date
-  $GITHUB_WORKSPACE=(pwd).Path
-  try {
-      Write-Host -ForegroundColor Magenta "
-------------------
+    $START = Get-Date
+    $GITHUB_WORKSPACE=(Get-Location).Path
+    try {
+        Write-Host -ForegroundColor Magenta "
+---------------------------------------
 
 BUILDING LEFT SIDE
 
-------------------
+---------------------------------------
 "
-      west build --pristine -s zmk/app -b corne-ish_zen_left -- -DZMK_CONFIG="${GITHUB_WORKSPACE}/config"
-      Copy-Item build/zephyr/zmk.uf2 firmware/corneish_zen_left.uf2
-      Write-Host -ForegroundColor Magenta "
-------------------
+        west build --pristine -s zmk/app -b corne-ish_zen_left -- -DZMK_CONFIG="${GITHUB_WORKSPACE}/config"
+        Copy-Item build/zephyr/zmk.uf2 firmware/corneish_zen_left.uf2 -ErrorAction Stop
+        Write-Host -ForegroundColor Magenta "
+---------------------------------------
 
 BUILDING RIGHT SIDE
 
-------------------
+---------------------------------------
 "
-    west build --pristine -s zmk/app -b corne-ish_zen_right -- -DZMK_CONFIG="${GITHUB_WORKSPACE}/config"
-  Copy-Item build/zephyr/zmk.uf2 firmware/corneish_zen_right.uf2
-  sudo cp firmware/* /mnt/e/Git/zen-mk/firmware
-  $END = Get-Date
-  Write-Host -ForegroundColor Magenta "
----------------------
-  Started : $START
-  Ended   : $END
-  Total   : $($END - $START)
----------------------"
+        west build --pristine -s zmk/app -b corne-ish_zen_right -- -DZMK_CONFIG="${GITHUB_WORKSPACE}/config"
+        Copy-Item build/zephyr/zmk.uf2 firmware/corneish_zen_right.uf2 -ErrorAction Stop
+        if (Test-Path '/mnt/e/Git/zen-mk/firmware') {
+            Get-ChildItem ./firmware -Filter *.uf2 | Copy-Item -Destination '/mnt/e/Git/zen-mk/firmware' -Verbose -ErrorAction Stop
+        }
     }
     catch {
         throw $_
+    }
+    finally {
+        $END = Get-Date
+        Write-Host -ForegroundColor Cyan "
+------------------------------------------
+    Started : $START
+    Ended   : $END
+    Total   : $($END - $START)
+------------------------------------------
+"
+
     }
 }
 
