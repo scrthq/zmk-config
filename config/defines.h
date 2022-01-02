6fcea5dacbae7dc0ce69bee011ca59efd87d8381
Nate Ferrell <nate@scrthq.com>
2022-01-02 02:47:38 -0600

    updateded keymap and migrated core defines off keymap

---
+++ config/defines.h
@@ -0,0 +1,30 @@
+/*
+*
+* Copyright (c) 2021 scrthq
+* SPDX-License-Identifier: MIT
+*
+*/
+
+#include <dt-bindings/zmk/keys.h>
+#include <dt-bindings/zmk/bt.h>
+
+#if (__has_include("local_macros.dtsi"))
+    #include "local_macros.dtsi"
+#else
+    #define mac_rtyu <&kp R>, <&kp T>, <&kp Y>, <&kp U>
+    #define mac_gh <&kp G>, <&kp H>
+    #define test_zeroes <&kp T>
+    #define mac_tl <&kp T>, <&kp L>
+#endif
+
+#define Z_MEH LS(LA(LCTL))
+
+#define WNDOWS 0
+#define MACOSX 1
+#define WINKEY 2
+#define MACKEY 3
+#define GAMING 4
+#define NUMPAD 5
+#define NUMROW 6
+#define ADJUST 7
+#define CONFIG 8
